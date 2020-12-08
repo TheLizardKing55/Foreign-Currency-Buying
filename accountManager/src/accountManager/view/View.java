559e544b696cfff3beb1423b7f7a7d1a0b02c9fd
TheLizardKing55 <49215772+TheLizardKing55@users.noreply.github.com>
2020-12-07 22:52:18 -0600

    Delete View.java

--- accountManager/src/accountManager/view/View.java
+++
@@ -1,9 +0,0 @@
-package accountManager.view;
-import accountManager.controller.Controller;
-import accountManager.model.Model;
-public interface View {
-	Controller getController();
-	public void setController(Controller aController);
-	public Model getModel();
-	public void setModel(Model aModel);
-}
